a8cb0564c8e9c5faaa1addaf69a936376762f01d
Md Milon <abireshan32@gmail.com>
2025-04-11 19:52:16 +0600

    add postgreSQL problem

--- assignment.sql
+++ assignment.sql
@@ -52,3 +52,41 @@ INSERT INTO orders (id, customer_id, book_id, quantity, order_date) VALUES
 (2, 2, 1, 1, '2024-02-20'),
 (3, 1, 3, 2, '2024-03-05');
 
+--problem-1:
+SELECT * FROM book
+    WHERE stock = 0;
+
+--problem-2:
+ SELECT * FROM book
+ ORDER BY price DESC LIMIT 1;
+
+--problem-3:
+SELECT customers.name, SUM(orders.quantity) AS total_orders
+FROM orders
+JOIN customers ON orders.customer_id = customers.id
+GROUP BY customers.name;
+
+
+--problem-4:
+SELECT SUM(book.price* orders.quantity) as total_revenue FROM orders
+ JOIN book ON orders.book_id = book.id;
+
+
+--problem-5:
+SELECT customers.name, SUM(orders.quantity) AS total_orders
+FROM orders
+JOIN customers ON orders.customer_id = customers.id
+GROUP BY customers.name
+HAVING SUM(orders.quantity) > 1;
+
+--problem-6:
+SELECT round(avg(price),2) as avg_book_price from  book;
+
+--problem-7--update the price of all books published before 2000 by 10%:
+UPDATE book
+SET price=price*1.10
+WHERE published_year<2000;
+
+--problem-8--delete all who did not place any orders:
+DELETE FROM customers
+WHERE id NOT IN (SELECT DISTINCT customer_id FROM orders);
