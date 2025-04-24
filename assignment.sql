a2ad45f03322928ed4cd6abf9308a95c2c4fd03f
Md Milon <abireshan32@gmail.com>
2025-04-24 20:25:20 +0600

    add database

--- assignment.sql
+++ assignment.sql
@@ -1,63 +1,6 @@
 -- Active: 1744364707698@@127.0.0.1@5432@bookstore_db
 -- create book table
-CREATE Table books(
-    id serial PRIMARY KEY,
-    title varchar(255) NOT NULL,
-    author varchar(255) NOT NULL,
-    published_year INT NOT NULL,
-    stock INTEGER NOT NULL,
-    price numeric(10, 2) NOT NULL
-);
--- DROP TABLE books;
--- DROP TABLE customers;
--- DROP TABLE orders;
- SELECT * from books;
 
--- create customer table
-CREATE TABLE customers(
-    customer_id serial PRIMARY KEY,
-    name varchar(255) NOT NULL,
-    email varchar(255) NOT NULL UNIQUE,
-    joined_date date NOT NULL
-);
-SELECT * from customers;
-
--- create orders table
-CREATE TABLE orders (
-    id SERIAL PRIMARY KEY,
-    customer_id INTEGER REFERENCES customers(customer_id) ON DELETE CASCADE,
-    book_id INTEGER REFERENCES books(id) ON DELETE CASCADE,
-    quantity INTEGER NOT NULL CHECK (quantity > 0),
-    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-SELECT * from orders;
-
--- //! -- 1. Insert a new book into the book table.
-
-INSERT INTO books (id, title, author, price, stock, published_year) VALUES
-(1, 'The Pragmatic Programmer', 'Andrew Hunt', 40.00, 10, 1999),
-(2, 'Clean Code', 'Robert C. Martin', 35.00, 5, 2008),
-(3, 'You Don''t Know JS', 'Kyle Simpson', 30.00, 8, 2014),
-(4, 'Refactoring', 'Martin Fowler', 50.00, 3, 1999),
-(5, 'Database Design Principles', 'Jane Smith', 20.00, 0, 2018);
-
--- //! -- 2. Insert a new customer into the customers table.
-INSERT INTO customers ( name, email, joined_date) VALUES
-( 'Alice', 'alice@email.com', '2023-01-10'),
-( 'Bob', 'bob@email.com', '2022-05-15'),
-( 'Charlie', 'charlie@email.com', '2023-06-20');
-
-SELECT * FROM customers;
-
--- //! -- 3. Insert a new order into the orders table.
-INSERT INTO orders (id, customer_id, book_id, quantity, order_date) VALUES
-(1, 1, 2, 1, '2024-03-10'),
-(2, 2, 1, 1, '2024-02-20'),
-(3, 1, 3, 2, '2024-03-05');
-
-
- SELECT * FROM orders;
 --problem-1:
 SELECT * FROM books
     WHERE stock = 0;
@@ -67,10 +10,11 @@ SELECT * FROM books
  ORDER BY price DESC LIMIT 1;
 
 --problem-3:
-SELECT customers.name, SUM(orders.quantity) AS total_orders
+SELECT name, SUM(quantity) AS total_orders
 FROM orders
-JOIN customers ON orders.customer_id = customers.customer_id
-GROUP BY customers.name;
+JOIN customers USING (customer_id)
+GROUP BY name;
+
 
 
 --problem-4:
@@ -79,11 +23,12 @@ SELECT SUM(books.price* orders.quantity) as total_revenue FROM orders
 
 
 --problem-5:
-SELECT customers.name, SUM(orders.quantity) AS total_orders
+SELECT name, SUM(quantity) AS total_orders
 FROM orders
-JOIN customers ON orders.customer_id = customers.customer_id
-GROUP BY customers.name
-HAVING SUM(orders.quantity) > 1;
+JOIN customers USING (customer_id)
+GROUP BY name
+HAVING SUM(orders.quantity)> 1;
+;
 
 --problem-6:
 SELECT round(avg(price),2) as avg_book_price from  books;
@@ -95,6 +40,6 @@ WHERE published_year<2000;
 
 --problem-8--delete all who did not place any orders:
 DELETE FROM customers
-WHERE id
+WHERE customer_id
  NOT IN 
 (SELECT DISTINCT customer_id FROM orders);
